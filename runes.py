2c2b1b3abf716b3784a1c3c8cd77c87e98a6f3ca
Nicolas Magee Rosado <nicolasmageerosado@gmail.com>
2025-05-01 13:09:46 +0200

    castle

--- runes.py
+++ runes.py
@@ -4,28 +4,58 @@ import functools
 # The actual spells the players see when they decode the rune spell
 # They all need to be the same length
 spell_phrases = """
-    @book##
-    @thing#
-    @soup##
-    @box###
-    @plany#
-    @fun###
+    @brave#soul!
+    @heroic#act!
+    @savior!!!!!
+    @masked#one!
+    @rescue!!!!!
+    @legend!!!!!
+    @true#gri!!!
+    @avenger!!!!
+    @true#fort!!
+    !!!!!!!!!!!!
+    @pixie#dust!
+    @fey#folk!!!
+    @glamour!!!!
+    @elf#light!!
+    @sprite!!!!!
+    @fair#one!!!
+    @will#o#wisp
+    @moss#child!
+    @dew#drops!!   
+    !!!!!!!!!!!!
+    @dark#lord!!
+    @nemesis!!!!
+    @evil#queen!
+    @mad#king!!!
+    @the#fiend!!
+    @doom#bringe
+    @wicked#one!
+    @menace!!!!!
+    @corruption!
+    !!!!!!!!!!!!
 """.split()
 
 spell_len = len(spell_phrases[0])
 spell_count = len(spell_phrases)
+print(f"{spell_len=} {spell_count=}")
 
 # The list of the runes that encrypt a particular rune
 # Could be a list...
+
 dep_list = {
-    0: (1,),
-    1: (2, 4),
-    2: (),
-    3: (4,),
-    4: (5,),
-    5: (),
+    i: [i + 1]
+    for i in range(29)
 }
 
+for i in (9, 19, 29):
+    dep_list[i] = []
+
+dep_list[7].append(19)    
+dep_list[18].append(8)    
+dep_list[28].append(18)
+
+print(dep_list)
 # From a to z 
 # alphabet_a_z = list((chr(ord('a') + i) for i in range(26))))
 
@@ -81,7 +111,17 @@ decoded_encrypted_spell_phrases = [
 
 # print('\n'.join(decoded_encrypted_spell_phrases))
 
+real_idx_to_public_idx = list(range(spell_count))
+
+random.seed(42)
+random.shuffle(real_idx_to_public_idx)
+public_idx_to_real_idx = {
+    real_idx_to_public_idx[i]: i
+    for i in range(spell_count)
+}
+
 for (i, decoded) in enumerate(decoded_encrypted_spell_phrases):
-    print(i, decoded)
+    print(f"{i:2} ({real_idx_to_public_idx[i]:2})  {decoded}")
     
-    
+for public in range(spell_count):
+    print(f"p {public:2}: {public_idx_to_real_idx[public]:2}")
